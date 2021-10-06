b9731d0df8883dd69af1a3238c14bd32df904060
ranaviira <84rent@gmail.com>
2021-10-06 16:41:44 +0400

    TextService Test passed

--- src/main/java/com/epam/izh/rd/online/Main.java
+++ src/main/java/com/epam/izh/rd/online/Main.java
@@ -1,22 +1,11 @@
 package com.epam.izh.rd.online;
 
-import java.math.BigInteger;
-import java.util.ArrayList;
+
 
 public class Main {
     public static void main(String[] args) {
-
-        ArrayList<BigInteger> list = new ArrayList<>();
-
-
-        int i = 2;
-        while (list.size() <= 100){
-            if(new BigInteger(String.valueOf(i)).isProbablePrime(i)){
-                list.add(BigInteger.valueOf(i));
-            }
-            i++;
-        }
-        System.out.println(list);
+        String s = "HAJKSdhalsjdh asajdhlajsaSJAHSKAD";
+        System.out.println(s.toLowerCase());
     }
 }
 
